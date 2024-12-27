3bd992593a58faa16566062fe36c80cf6d30bd8a
Ihor Tarknaiev <13971793it@gmail.com>
2024-12-27 14:45:20 +0100

    create admin module

---
+++ src/Resources/app/administration/src/module/ihor-test/index.js
@@ -0,0 +1,10 @@
+Shopware.module.register('ihor-test', {
+    type: 'plugin', //core or plugin - 2 Options, so always use plugin
+    name: 'MyPluginName', // Unique
+    // title: 'swag-example.general.mainMenuItemGeneral',
+    title: 'PageCoolTitle',
+    // description: 'sw-property.general.descriptionTextModule',
+    description: 'Some Really cooooool description',
+    color: '#ff3d58',
+    icon: 'regular-AR',
+});
