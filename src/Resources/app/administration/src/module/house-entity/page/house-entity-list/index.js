b7a9da47e69fbd316f2906924dc8decb1bd20754
Ihor Tarknaiev <13971793it@gmail.com>
2025-05-20 18:44:14 +0200

    Add Filter

--- src/Resources/app/administration/src/module/house-entity/page/house-entity-list/index.js
+++ src/Resources/app/administration/src/module/house-entity/page/house-entity-list/index.js
@@ -1,21 +1,21 @@
 import template from './house-entity-list.html.twig';
 
-const { Component, Context } = Shopware;
+const { Component, Context, Mixin } = Shopware;
 const { Criteria } = Shopware.Data;
 
 Component.register('house-entity-list', {
     template,
 
     inject: [
-      'repositoryFactory'
+        'repositoryFactory',
+        'filterFactory'
     ],
 
-    data() {
-        return {
-            repository: undefined,
-            houses: undefined
-        }
-    },
+    mixins: [
+        Mixin.getByName('notification'),
+        Mixin.getByName('listing'),
+        Mixin.getByName('placeholder')
+    ],
 
     metaInfo() {
         return {
@@ -23,10 +23,61 @@ Component.register('house-entity-list', {
         };
     },
 
+    data() {
+        return {
+            term: undefined,
+            total: 0,
+            page: 1,
+            limit: 30,
+            isLoading: false,
+            repository: undefined,
+            houses: undefined,
+            storeKey: 'grid.filter.house-filters',
+            defaultFilters: [
+                'my-house-filter'
+            ],
+            activeFilterNumber: 0,
+            filterCriteria: [],
+        }
+    },
+
     computed: {
         columns() {
             return this.getColumns();
-        }
+        },
+
+        listFilters() {
+            return this.filterFactory.create('house', {
+                'my-house-filter': {
+                    type: 'string-filter',
+                    property: 'number',
+                    label: 'Label',
+                    placeholder: 'Placeholder',
+                }
+            });
+        },
+
+        defaultCriteria() {
+            const defaultCriteria = new Criteria(this.page, this.limit);
+
+            defaultCriteria.setTerm(this.term);
+            defaultCriteria.addSorting(Criteria.sort('createdAt', 'DESC'));
+
+            this.filterCriteria.forEach(filter => {
+                defaultCriteria.addFilter(filter);
+            });
+
+            return defaultCriteria;
+        },
+    },
+
+    watch: {
+        defaultCriteria: {
+            handler() {
+                this.getList();
+            },
+            deep: true,
+        },
     },
 
     mounted() {
@@ -57,6 +108,31 @@ Component.register('house-entity-list', {
                     align: 'center',
                 }
             ];
-        }
+        },
+
+        updateCriteria(criteria) {
+            this.page = 1;
+            this.filterCriteria = criteria;
+        },
+
+        async getList() {
+            this.isLoading = true;
+
+            const criteria = await Shopware.Service('filterService')
+                .mergeWithStoredFilters(this.storeKey, this.defaultCriteria);
+
+            this.activeFilterNumber = criteria.filters.length;
+
+            try {
+                const items = await this.repository.search(criteria);
+
+                this.total = items.total;
+                this.houses = items;
+                this.isLoading = false;
+                this.selection = {};
+            } catch {
+                this.isLoading = false;
+            }
+        },
     },
 });
